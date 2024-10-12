e844ea32a4e70ca39efdd058069ee1f68cf19ac9
patricklimax <patricklimax@outlook.com.br>
2024-10-11 23:30:33 -0300

    style: update style theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,35 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter, Outfit } from 'next/font/google';
+import './globals.css';
+import Footer from '../components/footer';
+import Header from '../components/header';
 
 const inter = Inter({
-  subsets: ["latin"],
-  display: "swap",
+	subsets: ['latin'],
+	display: 'swap'
 });
 
+const outfit = Outfit({ subsets: ['latin'] });
+
 export const metadata: Metadata = {
-  title: "Chocolade Store",
-  description: "Seu melhor sabor de chocolate. Experimente!",
+	title: 'Cioccolato Store',
+	description: 'Seu melhor sabor de chocolate. Experimente!'
 };
 
 export default function RootLayout({
-  children,
+	children
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="pt-BR">
-      <body className={`${inter} antialiased`}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang='pt-BR'>
+			<body className={`${inter} antialiased`}>
+				<div className='max-w-[88rem] mx-auto min-h-screen'>
+					<Header />
+					<main className='max-w-7xl mx-auto'>{children}</main>
+					<Footer />
+				</div>
+			</body>
+		</html>
+	);
 }
