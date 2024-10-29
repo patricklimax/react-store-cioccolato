7365d2fc91c6a74b70abf6172bfee0a1f5cbade2
patricklimax <patricklimax@outlook.com.br>
2024-10-28 23:50:15 -0300

    chore: update logotipo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,12 +5,12 @@ export const Logotipo = () => {
 	return (
 		<Link href='/'>
 			<Image
-				src='/images/logo/logo.png'
+				src='/images/logo/logotipo.svg'
 				alt='Logo da Empresa'
 				height={60}
 				width={204}
 				quality={100}
-				className='w-[204px] h-[60px]'
+				className='w-[204px] h-[60px] drop-shadow-2xl shadow-red-600'
 				priority
 				sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
 			/>
